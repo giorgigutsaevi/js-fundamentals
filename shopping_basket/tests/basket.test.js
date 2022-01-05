bc999cb23258795124168a21e0ce1d0d33183291
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-05 16:20:33 +0000

    Refactoring: forgot to add addItem() method tests

--- shopping_basket/tests/basket.test.js
+++ shopping_basket/tests/basket.test.js
@@ -8,6 +8,19 @@ describe("ShoppingBasket object", () =>{
 		expect(testBasket).toBeInstanceOf(ShoppingBasket)
 	});
 
+
+	it("has addItem() method", ()=>{
+		const test = new ShoppingBasket();
+		expect(typeof test.addItem).toBe("function")
+	})
+
+	it("addItem() adds the desired candy into the basket's array", ()=>{
+		const test = new ShoppingBasket();
+		test.addItem(new Candy('Skittles', 12))
+		test.addItem(new Candy('M&Ms', 7))
+		expect(test.basketArray.length).toEqual(2)
+	})
+
 	it("has getTotalPrice() method", ()=>{
 		const test = new ShoppingBasket();
 		expect(typeof test.getTotalPrice).toBe("function")
