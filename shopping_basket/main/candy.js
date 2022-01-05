807a63ef6e09598317b13f6d01e1312f43aba289
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-05 16:00:13 +0000

    Test drove the implementation of getName() and getPrice() for the Candy Object

---
+++ shopping_basket/main/candy.js
@@ -0,0 +1,22 @@
+
+class Candy{
+	constructor(candyName, price){
+		this.candyName = candyName;
+		this.price = price;
+	}
+
+	getName(){
+		return this.candyName
+	}
+
+	getPrice(){
+		return this.price
+	}
+
+}
+
+// let myCandy1 = new Candy('Maltesers', 1.99);
+// console.log(myCandy1.getName());
+// console.log(myCandy1.getPrice());
+
+module.exports = Candy;
---
+++ shopping_basket/tests/candy.test.js
@@ -0,0 +1,30 @@
+const Candy = require("../main/candy")
+
+describe("Candy object", () => {
+	it("creates an instance of itself", ()=>{
+		const testCandy = new Candy('Maltesers', 3.99);
+		expect(testCandy).toBeInstanceOf(Candy)
+	});
+
+	it("has getName() method", ()=>{
+		const testCandy = new Candy('Maltesers', 3.99);
+		expect(typeof testCandy.getName).toBe("function")
+	});
+
+	it("getName() returns the candy's name", ()=>{
+		const maltesers = new Candy('Maltesers', 3.99);
+		expect(maltesers.getName()).toBe("Maltesers")
+	});
+
+	it("has getPrice() method", ()=>{
+		const kitkat = new Candy('Kitkat', 2.99);
+		expect(typeof kitkat.getPrice).toBe("function")
+	})
+
+	it("getPrice() returns the candy's price", ()=>{
+		const kitkat = new Candy('Kitkat', 2.99);
+		expect(kitkat.getPrice()).toBe(2.99)
+	})
+
+})
+
